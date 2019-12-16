5d8e1f6b1942f4ac419b2e642526f7d3914d94a8
Sergio <45538277+sergiom19@users.noreply.github.com>
2019-12-16 18:26:57 +0100

    Ver la lista de usuarios agregados

---
+++ comandos/verusuarios.js
@@ -0,0 +1,22 @@
+exports.run = async (client, message, args) => {
+
+const usuarios_db = new megadb.crearDB("usuarios") //Requerimos la misma database que hemos creado.
+
+    if(usuarios_db.size() < 1) return message.channel.send('No hay usuarios en la lista.');//Si no hay usuarios en la db esto envia una mensaje.
+    
+    //Ahora la palabra 'key' es el nombre principal del JSON ej:
+    //Este seria la database en JSON
+    //{
+  //"478572042384572424": { //Esto es key siempre, los demas subdatos son v = valor, si requerimos uno ponemos 'v.discord_nombre' porque le pusimos 'discord_nombre'
+    //"discord_nombre": "sergiodiscord#1032",
+    //"discord_servidor": "Sergio's Project"
+  //}
+//}
+    //Comemzamos con la busqueda de la lista
+    usuarios_db.map(false, (v, key) => `Usuario ID: ${key}\nNombre: ${v.discord_nombre}\nServidor: ${v.discord_servidor}`).then(lista_usuarios => {
+      return message.channel.send(lista_usuarios.join("\n"))
+      
+      //Esto hara que separe cada usuario con la siguiente linea, para que no se acomulen.
+    }) 
+    
+};
