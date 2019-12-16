b267026b4a8d5bcd7124048574a160191a9313a6
Sergio <45538277+sergiom19@users.noreply.github.com>
2019-12-16 18:17:27 +0100

    Update agregarusuario.js

--- comandos/agregarusuario.js
+++ comandos/agregarusuario.js
@@ -10,15 +10,10 @@ exports.run = async (client, message, args) => {
 		if (usuarios_db.tiene(`${sujeto.id}`))
 			return message.reply(`${sujeto} ya esta agregado a la lista.`);
 		usuarios_db.establecer(`${sujeto.id}`, {
-    nombre_discord
-    });
-		message.channel.send(embed.setDescription('<@' + sujeto.id + '> ha sido agregado al staff.'));
-		sujeto.send(
-			embed.setDescription(
-				message.author +
-					' te ha agregado como staff para el bot.\n - Usa los comandos adecuadamente.'
-			)
-		);
+    		nombre_discord: sujeto.tag, //ej. sergiodiscord#1032
+		nombre_servidor: message.guild.name //ej. Sergio's projects	
+    			});
+		message.channel.send('<@' + sujeto.id + '> ha sido agregado a la lista.');
 
 
-}
+};
