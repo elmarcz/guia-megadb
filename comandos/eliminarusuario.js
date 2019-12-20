b83151a8a0ae79abafa02689b860d4a13e0afec1
Sergio <45538277+sergiom19@users.noreply.github.com>
2019-12-20 17:59:31 +0100

    Delete eliminarusuario.js

--- comandos/eliminarusuario.js
+++
@@ -1,9 +0,0 @@
-exports.run = async (client, message, args) => {
-
-  const usuarios_db = new megadb.crearDB("usuarios")
-  let sujeto = message.mentions.users.first()
-  if(!usuarios_db.tiene(`${sujeto.id}`)) return message.reply("ese usuario no esta en la lista.") //Si el usuario no esta en la lista manda el mensaje y no sucede nada.
-  usuarios_db.eliminar(`${sujeto.id}`)//Se elimina el usuario de la lista.
-  return message.channel.send(sujeto+" ha sido eliminado de la lista.");
-
-};
